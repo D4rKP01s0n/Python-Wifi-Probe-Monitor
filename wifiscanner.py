05a2a77cf18ddf07195ec27beb7387045ca75c8d
D4rKP01s0n <archergarcia2@gmail.com>
2016-11-13 10:50:13 -0500

    Update wifiscanner.py

--- wifiscanner.py
+++ wifiscanner.py
@@ -2,8 +2,6 @@
 #     Wifiscanner.py - A simple python script which records and logs wifi probe requests.
 #     Author - D4rKP01s0n
 #     Requirements - Scapy and Datetime
-#     Inspiration - Tim Tomes (LaNMaSteR53)'s WUDS https://bitbucket.org/LaNMaSteR53/wuds/
-#     Reminder - Change mon0 (around line 65) to your monitor-mode enabled wifi interface
 #########################################################################
 
 
@@ -12,11 +10,15 @@ from scapy.all import sniff, Dot11
 #import numpy
 import logging
 import time
+import notify.py
 #Devices which are known to be constantly probing
 IGNORE_LIST = set(['00:00:00:00:00:00', '01:01:01:01:01:01'])
-SEEN_DEVICES = set() #Devices which have had their probes recieved
+#SEEN_DEVICES = set() #Devices which have had their probes recieved, removed due to memory use
 d = {'00:00:00:00:00:00':'Example MAC Address'} #Dictionary of all named devices
 
+
+wificard = 'mon0' #    Change this to your monitor-mode enabled wifi interface
+notifications = False #     Change to True and set up in notify.py if you want Pushover notifications
 
 #knownfile = open('knowndevices.txt', 'a')
 #knownfile.write(str(d))
@@ -41,7 +43,7 @@ def handle_packet(pkt):
 		#logging.debug('Probe Recorded with MAC ' + curmac)
 		curmac = pkt.addr2
 		curmac = curmac.upper() #Assign variable to packet mac and make it uppercase
-		SEEN_DEVICES.add(curmac) #Add to set of known devices (sets ignore duplicates so it is not a problem)
+		#SEEN_DEVICES.add(curmac) #Add to set of known devices (sets ignore duplicates so it is not a problem)
 		if curmac not in IGNORE_LIST: #If not registered as ignored
 			if curmac in d:
 				logging.info('\033[95m' + 'Probe Recorded from ' + '\033[93m' + d[curmac] + '\033[95m' + ' with MAC ' + curmac + '\033[0m') #Log to file wifiscanner.log with purple color
@@ -51,6 +53,9 @@ def handle_packet(pkt):
 				logging.info('\033[92m' + 'Probe Recorded from MAC ' + pkt.addr2 + '\033[0m') #Log to file wifiscanner.log with green color
 				print('\033[95m' + 'Device MAC: {pkt.addr2} '
 					'with SSID: {pkt.info}'.format(pkt=pkt) + '\033[0m') #Print to command line with green color
+				if lastnotify != curmac  #  So that notifications don't repeat
+					notify("Unknown device with MAC %s sighted!") % curmac    # Notify via Pushover
+					lastnotify = curmac
 			#print SEEN_DEVICES #Just for debug, prints all known devices
 			#dump()
 
@@ -62,11 +67,9 @@ def main():
 	
 	import argparse
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--interface', '-i', default='mon0', # Change mon0 to your monitor-mode enabled wifi interface
+	parser.add_argument('--interface', '-i', default=wificard, # Change mon0 to your monitor-mode enabled wifi interface
 				help='monitor mode enabled interface')
 	args = parser.parse_args()
-	sniff(iface=args.interface, prn=handle_packet) #start sniffin
-	while 1:
-		time.sleep(1) # Supposed to make an infinite loop, but for some reason it stops after a while
+	sniff(iface=args.interface, prn=handle_packet, store=0, count=0) #start sniffin, store=0 makes sniff not save captured packets and count=0 makes sniff continously scan packets
 if __name__ == '__main__':
 	main()
